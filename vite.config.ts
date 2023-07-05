d42e50c399e12e6c7aa3720f52c90919fb5a5cb3
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-Air.home>
2023-07-05 13:56:09 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 4200
-	},
+	// server: {
+	// 	port: 4200
+	// },
 	build: {
 		outDir: 'build'
 	},
